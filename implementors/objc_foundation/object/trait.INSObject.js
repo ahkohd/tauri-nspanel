77c265ead728457f18181b815105b000367519be
ahkohd <Victor.olorunbunmi@gmail.com>
2023-04-02 09:06:04 +0000

    Deploying to gh-pages from @ ahkohd/tauri-nspanel@3c5667cf358a7732d187231cc0525afb8df1cde6 🚀

--- implementors/objc_foundation/object/trait.INSObject.js
+++ implementors/objc_foundation/object/trait.INSObject.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "objc_foundation":[],
-"tauri_nspanel":[["impl <a class=\"trait\" href=\"objc_foundation/object/trait.INSObject.html\" title=\"trait objc_foundation::object::INSObject\">INSObject</a> for <a class=\"struct\" href=\"tauri_nspanel/raw_nspanel/struct.RawNSPanel.html\" title=\"struct tauri_nspanel::raw_nspanel::RawNSPanel\">RawNSPanel</a>"]]
+"tauri_nspanel":[["impl&lt;R:&nbsp;<a class=\"trait\" href=\"tauri/trait.Runtime.html\" title=\"trait tauri::Runtime\">Runtime</a>&gt; <a class=\"trait\" href=\"objc_foundation/object/trait.INSObject.html\" title=\"trait objc_foundation::object::INSObject\">INSObject</a> for <a class=\"struct\" href=\"tauri_nspanel/raw_nspanel/struct.RawNSPanel.html\" title=\"struct tauri_nspanel::raw_nspanel::RawNSPanel\">RawNSPanel</a>&lt;R&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/os/fd/owned/trait.AsFd.js
+++ implementors/std/os/fd/owned/trait.AsFd.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {
 "io_lifetimes":[],
-"rustix":[["impl&lt;'fd&gt; <a class=\"trait\" href=\"rustix/fd/trait.AsFd.html\" title=\"trait rustix::fd::AsFd\">AsFd</a> for <a class=\"struct\" href=\"rustix/io/struct.PollFd.html\" title=\"struct rustix::io::PollFd\">PollFd</a>&lt;'fd&gt;"]]
+"rustix":[["impl&lt;'fd&gt; <a class=\"trait\" href=\"rustix/fd/trait.AsFd.html\" title=\"trait rustix::fd::AsFd\">AsFd</a> for <a class=\"struct\" href=\"rustix/io/struct.PollFd.html\" title=\"struct rustix::io::PollFd\">PollFd</a>&lt;'fd&gt;"]],
+"tokio":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"tokio/fs/struct.File.html\" title=\"struct tokio::fs::File\">File</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
