747b69333ac70da14632991a6a615d2083d40601
ahkohd <ahkohd@users.noreply.github.com>
2026-09-19 17:38:03 +0000

    Deploying to gh-pages from @ ahkohd/tauri-nspanel@d612c4f8b288182ad017548ebcaf2bd784b659e3 🚀

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["tauri_nspanel",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"tauri_nspanel/builder/enum.ResizeDirection.html\" title=\"enum tauri_nspanel::builder::ResizeDirection\">ResizeDirection</a>",0]]]]);
+    const implementors = Object.fromEntries([["tauri_nspanel",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"tauri_nspanel/builder/enum.ResizeDirection.html\" title=\"enum tauri_nspanel::builder::ResizeDirection\">ResizeDirection</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"tauri_nspanel/enum.StyleMaskError.html\" title=\"enum tauri_nspanel::StyleMaskError\">StyleMaskError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[305]}
+//{"start":59,"fragment_lengths":[571]}
